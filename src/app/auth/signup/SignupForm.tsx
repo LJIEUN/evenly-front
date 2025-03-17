b5da9a06a800d8ae37e6a96e8c33106c40de888b
SangHwi <brilyent72@gmail.com>
2025-03-17 14:15:42 +0900

    회원 가입 페이지 기본 틀 생성

--- src/app/auth/signup/SignupForm.tsx
+++ src/app/auth/signup/SignupForm.tsx
@@ -1,1 +1,12 @@
-export default function SignupForm() {}
+"use client";
+import { useState } from "react";
+
+export default function SignupForm() {
+
+    return (
+        <form className="p-6 border rounded-lg shadow-md w-96">
+
+        </form>
+
+    );
+}
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -2,5 +2,9 @@ import SignupForm from "./SignupForm";
 
 export default function SignupPage() {
 
-
-    }
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <SignupForm />
+        </div>
+    );
+}
