e529b9374fe6c16c5290724bb5e8a4c96aa4daed
SangHwi <brilyent72@gmail.com>
2025-03-17 17:56:19 +0900

    회원 가입 페이지 UI 임시 디자인 (구름디자인시스템 적용 전)

--- src/app/auth/signup/SignupForm.tsx
+++ src/app/auth/signup/SignupForm.tsx
@@ -3,10 +3,26 @@ import { useState } from "react";
 
 export default function SignupForm() {
 
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [message, setMessage] = useState("");
+
     return (
-        <form className="p-6 border rounded-lg shadow-md w-96">
-
-        </form>
-
+        <div id="contents" className="pt-[170px] pb-[80px] px-[25px] max-w-[400px] mr-[255px]">
+            <form className="w-[400px]">
+                <h3 className="mb-[15px] pb-[30px]">create account</h3>
+                {message && <p className="text-red-500">{message}</p>}
+                <label className="block text-gray-700 mt-[25px] mb-[5px]">userId</label>
+                <input type="email" value={userId} onChange={(e) => setUserId(e.target.value)} className="border bg-white p-2 w-full" required />
+                <span className="text-[11px]">아이디를 입력해 주세요.</span>
+                <label className="block text-gray-700 mt-[25px] mb-[5px]">Password*</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border bg-white p-2 w-full" required />
+                <span className="text-[11px]">(영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 8자~16자)</span>
+                <label className="block text-gray-700 mt-[25px] mb-[5px]">Password check*</label>
+                <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="border bg-white p-2 w-full" required />
+                <button type="submit" className="mt-[40px] mb-[5px] bg-black text-white font-bold px-4 py-2 rounded w-full">create account</button>
+            </form>
+        </div>
     );
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -3,7 +3,7 @@ import SignupForm from "./SignupForm";
 export default function SignupPage() {
 
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center h-screen bg-[#f5f3ef]">
             <SignupForm />
         </div>
     );
