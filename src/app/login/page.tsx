442fe7f849fb076af9826b5bb39c9af30227984f
seongmin <min@seongminui-MacBookPro.local>
2025-03-18 18:12:08 +0900

    로그인 폼 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
-"use client";
-
+import LoginForm from "@/components/auth/LoginForm/LoginForm";
 import React from "react";
 
 const LoginPage = () => {
 	return (
-		<div className="flex flex-col">
-			<div>로그인 페이지</div>
+		<div className="flex flex-col flex-1 h-full w-full max-w-md">
+			<h1 className="text-xl mb-20">Login</h1>
+			<div className="flex-1">
+				<LoginForm />
+			</div>
 		</div>
 	);
 };
