e821a9f1aeda739b9eb06b92b810bf7b123b1246
seongmin <min@seongminui-MacBookPro.local>
2025-03-21 01:21:35 +0900

    홈일때만 사이드바가 보이도록 적용

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,23 @@
+"use client";
+
 import React, { ReactNode } from "react";
 import Header from "./Header/Header";
 import Sidebar from "./SideBar/Sidebar";
+import { usePathname } from "next/navigation";
 
 interface LayoutProps {
 	children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+	const pathname = usePathname();
+	const isHome = pathname === "/";
+
 	return (
 		<div className="h-screen flex flex-col overflow-hidden">
 			<Header />
 			<div className="flex flex-1 overflow-hidden">
-				<Sidebar />
+				{isHome && <Sidebar />}
 				<main className="flex flex-col flex-1 overflow-hidden container mx-auto px-4 py-8">
 					{children}
 				</main>
