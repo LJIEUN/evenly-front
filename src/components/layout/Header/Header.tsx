452256531d981b7a32364c50ca5ec1cb6448da2e
seongmin <min@seongminui-MacBookPro.local>
2025-03-21 01:21:21 +0900

    헤더에 마이페이지 추가

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -9,8 +9,13 @@ const Header = () => {
 					<h1 className="text-2xl font-bold cursor-pointer">Evenly</h1>
 				</Link>
 			</div>
-			<div className="flex items-start gap-4 py-3 ">
-				<span>Cart</span>
+			<div className="flex items-start gap-4 py-3">
+				<Link href="/" className="flex gap-2 px-4  cursor-pointer">
+					<span className="text-link">Cart</span>
+				</Link>
+				<Link href="/mypage" className="flex gap-2 px-4  cursor-pointer">
+					<span className="text-link">My Page</span>
+				</Link>
 				<Link href="/login" className="flex gap-2 px-4  cursor-pointer">
 					<span className="text-link">로그인</span>
 				</Link>
