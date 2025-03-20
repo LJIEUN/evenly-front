20842ce2c55cfef442d3d7736f0e198415d0c2fa
seongmin <min@seongminui-MacBookPro.local>
2025-03-20 23:37:28 +0900

    로그인 로직을 위한 props 추가

--- src/components/auth/LoginForm/LoginForm.tsx
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -1,13 +1,45 @@
+"use client";
 import BaseButton from "@/components/common/Button/BaseButton";
 import PasswordInput from "@/components/common/Input/PasswordInput";
 import TextInput from "@/components/common/Input/TextInput";
 import React from "react";
+import useLoginForm from "./useLoginForm";
 
-const LoginForm = () => {
+export interface LoginFromProps {
+	onSuccess?: () => void;
+	onError?: (error: Error) => void;
+}
+
+const LoginForm = ({ onSuccess, onError }: LoginFromProps) => {
+	const { formState, errors, handleFormChange, handleSubmit } = useLoginForm({
+		onSuccess,
+		onError,
+	});
+
 	return (
-		<form className="flex flex-col gap-4 w-full max-w-[40rem]" noValidate>
-			<TextInput placeholder="account ID" required size="xl" />
-			<PasswordInput placeholder="password" required size="xl" />
+		<form
+			onSubmit={handleSubmit}
+			className="flex flex-col gap-4 w-full max-w-[40rem]"
+			noValidate
+		>
+			<TextInput
+				placeholder="account ID"
+				value={formState.id}
+				required
+				size="xl"
+				onChange={handleFormChange("id")}
+				styleState={errors.id ? "invalid" : "default"}
+				error={errors.id}
+			/>
+			<PasswordInput
+				placeholder="password"
+				value={formState.password}
+				required
+				size="xl"
+				onChange={handleFormChange("password")}
+				styleState={errors.password ? "invalid" : "default"}
+				error={errors.password}
+			/>
 			<BaseButton type="submit" size="xl">
 				로그인
 			</BaseButton>
