63458e9551de3b7bdb156deea289566231d227ba
seongmin <min@seongminui-MacBookPro.local>
2025-03-21 01:15:53 +0900

    헤더 디자인 수정

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,9 +3,15 @@ import React from "react";
 
 const Header = () => {
 	return (
-		<header className="max-w-[120rem] w-full mx-auto h-[8rem] flex justify-between items-center p-4">
-			<div className="flex items-start gap-4">
-				<Link href="/login" className="flex gap-2 px-4 py-3 cursor-pointer">
+		<header className="max-w-[120rem] w-full mx-auto h-[4rem] flex justify-between items-center p-4">
+			<div>
+				<Link href="/">
+					<h1 className="text-2xl font-bold cursor-pointer">Evenly</h1>
+				</Link>
+			</div>
+			<div className="flex items-start gap-4 py-3 ">
+				<span>Cart</span>
+				<Link href="/login" className="flex gap-2 px-4  cursor-pointer">
 					<span className="text-link">로그인</span>
 				</Link>
 			</div>
