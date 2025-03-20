b1106cff33059a597862834251a75908a8394b13
seongmin <min@seongminui-MacBookPro.local>
2025-03-21 00:24:56 +0900

    아이디 글자수 제한, 비밀번호 특문조건 추가

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,8 +1,14 @@
+const ID_MIN_LENGTH = 3;
+const ID_MAX_LENGTH = 20;
 const PASSWORD_MIN_LENGTH = 8;
 
 export const validateId = (id: string): string | undefined => {
 	if (!id) {
 		return "아이디를 입력해주세요.";
+	}
+
+	if (id.length < ID_MIN_LENGTH || id.length > ID_MAX_LENGTH) {
+		return `비밀번호는 최소 ${ID_MIN_LENGTH}자 이상 ${ID_MAX_LENGTH}이하여야 합니다.`;
 	}
 };
 
@@ -25,5 +31,9 @@ export const validatePassword = (password: string): string | undefined => {
 
 	if (!/[0-9]/.test(password)) {
 		return "숫자를 포함해야 합니다.";
+	}
+
+	if (!/[!@#$%^&*]/.test(password)) {
+		return "특수문자(!@#$%^&*)를 포함해야 합니다.";
 	}
 };
