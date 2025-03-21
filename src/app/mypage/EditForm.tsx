a81ff1740f744c87ffa3c26331a0fae9b7116d6e
SangHwi <brilyent72@gmail.com>
2025-03-21 10:13:35 +0900

    마이페이지, 회원가입 수정 폼 디자인 변경

--- src/app/mypage/EditForm.tsx
+++ src/app/mypage/EditForm.tsx
@@ -12,16 +12,16 @@ export default function EditForm({ user, onCancel }: { user: { userId: string; n
         <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
             <h3 className="text-xl font-bold mb-4">비밀번호 변경</h3>
             {message && <p className="text-red-500">{message}</p>}
-            <form>
+            <form className="w-[300px] tracking-[2px]">
                 <p><strong>아이디:</strong> {user?.userId || "userId"}</p>
                 <p><strong>이름:</strong> {user?.name || "name"}</p>
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">새 비밀번호</label>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border bg-white p-2 w-full" required />
-                <label className="block text-gray-700 mt-[25px] mb-[5px]">비밀번호 확인</label>
+                <label className="block text-gray-700 mt-[10px] mb-[5px]">비밀번호 확인</label>
                 <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="border bg-white p-2 w-full" required />
-                <div className="flex gap-3 mt-4">
-                    <button type="submit" className="w-full bg-green-500 text-white py-2 rounded hover:bg-green-700">변경하기</button>
-                    <button type="button" className="w-full bg-gray-400 text-white py-2 rounded hover:bg-gray-600" onClick={onCancel}>취소</button>
+                <div className="flex gap-3 mt-[20px]">
+                    <button type="submit" className="w-full bg-[var(--soft-black)] text-white py-2 rounded hover:bg-black">변경하기</button>
+                    <button type="button" className="w-full bg-gray-600 text-white py-2 rounded hover:bg-gray-800" onClick={onCancel}>취소</button>
                 </div>
             </form>
         </div>
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -30,14 +30,12 @@ export default function MyPage() {
 
     return (
         <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
-            <div>
+            <div className="w-[250px] mt-[20px] tracking-[2px]">
                 <p><strong>아이디:</strong> {user?.userId || "userId"}</p>
                 <p><strong>이름:</strong> {user?.name || "name"}</p>
-                <div className="mt-4 flex flex-col gap-3">
-                    <label className="block text-gray-700 mt-[25px] mb-[5px]">회원 정보 수정</label>
-                    <button className="mb-[5px] bg-[#000000] text-[#fff] font-bold px-4 py-2 rounded w-[200px]" onClick={() => setView("edit")}>회원 정보 수정</button>
-                    <label className="block text-gray-700 mt-[25px] mb-[5px]">배송 정보 조회</label>
-                    <button className="mb-[5px] bg-[#000000] text-[#fff] font-bold px-4 py-2 rounded w-full" onClick={() => alert("배송 조회 기능은 추후 추가 예정입니다!")}>배송 조회</button>
+                <div className="mt-[40px] flex flex-col gap-3">
+                    <button className="h-[40px] bg-[var(--soft-black)] text-[#fff] font-bold px-4 py-1 rounded w-full hover:bg-black" onClick={() => setView("edit")}>회원 정보 수정</button>
+                    <button className="h-[40px] bg-[var(--soft-black)] text-[#fff] font-bold px-4 py-1 rounded w-full hover:bg-black" onClick={() => alert("배송 조회 기능은 추후 추가 예정입니다!")}>배송 조회</button>
                 </div>
             </div>
         </div>
