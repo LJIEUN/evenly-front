06918395486ff543b23f29c6b2bc4698851fb904
SangHwi <brilyent72@gmail.com>
2025-03-26 11:34:51 +0900

    회원 가입 페이지 CSS 수정

--- src/app/signup/SignupForm.tsx
+++ src/app/signup/SignupForm.tsx
@@ -15,14 +15,13 @@ export default function SignupForm() {
     return (
         <div id="contents" className="pt-[30px] pb-[30px] px-[25px] max-w-[400px] mr-[255px]">
             <form className="w-[400px]" onSubmit={handleSubmit}>
-                <h3 className="mb-[15px] pb-[30px]">create account</h3>
+                <h3 className="text-xl mb-[15px] pb-[30px]">create account</h3>
                 {message && <p className="text-red-500">{message}</p>}
-                <label className="block text-gray-700 mt-[25px] mb-[5px]">userId</label>
+                <label className="block text-gray-700 mt-[25px] mb-[5px]">ID*</label>
                 <input type="email" value={userId} onChange={(e) => setUserId(e.target.value)} className="border bg-white p-2 w-full" required />
-                <span className="text-[11px]">아이디를 입력해 주세요.</span>
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">Password*</label>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border bg-white p-2 w-full" required />
-                <span className="text-[11px]">(영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 8자~16자)</span>
+                <span className="text-[11px]">(영문 대문자/소문자/숫자/특수문자를 모두 포함, 8자 이상)</span>
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">Password check*</label>
                 <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="border bg-white p-2 w-full" required />
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">Name*</label>
