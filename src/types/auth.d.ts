e213b8c7cb9cc29f5e5dc504ba16236b4956d188
seongmin <min@seongminui-MacBookPro.local>
2025-03-26 11:59:21 +0900

    jwt 토큰 디코딩 코드 추가

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -12,3 +12,10 @@ export interface AuthError extends Error {
 	statusCode?: number;
 	code?: string;
 }
+
+export interface DecodedToken {
+	sub: string; // 유저 ID
+	role: string;
+	iat: number; // 생성 시간
+	exp: number; // 만료 시간
+}
