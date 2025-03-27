30dc9224ee5ca870b3721389061d592e60f44e69
SangHwi <brilyent72@gmail.com>
2025-03-27 19:17:49 +0900

    회원 가입 페이지 입력이 안되는 버그 수정

--- src/app/signup/SignupForm.tsx
+++ src/app/signup/SignupForm.tsx
@@ -16,14 +16,14 @@ export default function SignupForm() {
                 <h3 className="text-xl mb-[15px] pb-[30px]">create account</h3>
                 {message && <p className="text-red-500">{message}</p>}
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">ID*</label>
-                <input type="text" value={form.userId} onChange={handleChange} className="border bg-white p-2 w-full" required />
+                <input type="text" name="userId" value={form.userId} onChange={handleChange} className="border bg-white p-2 w-full" required />
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">Password*</label>
-                <input type="password" value={form.password} onChange={handleChange} className="border bg-white p-2 w-full" required />
+                <input type="password" name="password" value={form.password} onChange={handleChange} className="border bg-white p-2 w-full" required />
                 <span className="text-[11px]">(영문 대문자/소문자/숫자/특수문자를 모두 포함, 8자 이상)</span>
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">Password check*</label>
-                <input type="password" value={form.confirmPassword} onChange={handleChange} className="border bg-white p-2 w-full" required />
+                <input type="password" name="confirmPassword" value={form.confirmPassword} onChange={handleChange} className="border bg-white p-2 w-full" required />
                 <label className="block text-gray-700 mt-[25px] mb-[5px]">Name*</label>
-                <input type="text" value={form.name} onChange={handleChange} className="border bg-white p-2 w-full" required />
+                <input type="text" name="name" value={form.name} onChange={handleChange} className="border bg-white p-2 w-full" required />
                 <button type="submit" className="mt-[40px] mb-[5px] bg-black text-white font-bold px-4 py-2 rounded w-full">create account</button>
             </form>
         </div>
