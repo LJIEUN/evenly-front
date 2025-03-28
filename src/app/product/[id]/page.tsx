41cf9b606c1da4829f4ba8374e23210a701e8962
seongmin <min@seongminui-MacBookPro.local>
2025-03-28 15:19:08 +0900

    배포 위해서 오류 임시 주석처리

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -9,19 +9,19 @@ import React, { useEffect, useState } from "react";
 const ProductDetailPage = () => {
 	const { id } = useParams();
 	const [product, setProduct] = useState<Product | null>(null);
-	const [error, setError] = useState(false);
+	// const [error, setError] = useState(false);
 
 	useEffect(() => {
 		if (!id) return;
 		fetchProduct();
-	}, []);
+	});
 
 	const fetchProduct = async () => {
 		const data = await getProductById(Number(id));
 		setProduct(data);
 	};
 
-	if (error) return <div>존재하지 않는 상품</div>;
+	// if (error) return <div>존재하지 않는 상품</div>;
 	if (!product) return <div>로딩중</div>;
 
 	return (
